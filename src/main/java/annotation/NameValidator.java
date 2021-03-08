f4e46d25382bd709fe97bb2d333b80d43ea11ba0
rzanelato <rzanelato@leadtech.com>
2021-03-08 08:03:13 +0100

    remove import for NameValidator annotation

--- src/main/java/annotation/NameValidator.java
+++ src/main/java/annotation/NameValidator.java
@@ -1,33 +1,28 @@
 package annotation;
 
+import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-import java.util.Optional;
-
-public class NameValidator implements ConstraintValidator<IsValidName, String> {
+public class NameValidator {
 
     private String name;
     private boolean couldValidate;
     private boolean empty;
 
-    @Override
-    public void initialize(IsValidName constraintAnnotation) {
+     public void initialize(IsValidName constraintAnnotation) {
         this.name = constraintAnnotation.name();
         this.couldValidate = constraintAnnotation.couldValidate();
         this.empty = constraintAnnotation.empty();
     }
 
-    @Override
-    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+    public boolean isValid(String value) {
         boolean couldEmpty = empty && StringUtils.isBlank(value);
         return couldEmpty || isValidName(value);
     }
 
     private boolean isValidName(String value) {
         return Optional.ofNullable(value)
-                .filter(txt -> couldValidate && name.equals(txt))
-                .isPresent();
+                       .map(txt -> couldValidate && name.equals(txt))
+                       .orElse(true);
     }
 }
